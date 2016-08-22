4d8d634adcaaefc0db2a0b886e60cfa5b1d59aa6
Michail Yasonik <Michail Yasonik>
2016-08-21 21:26:38 -0700

    getting data in template

--- gulp/templates.js
+++ gulp/templates.js
@@ -1,20 +1,28 @@
 var $ = require('./common.js');
 var config = require('./config.js');
 
+var fs = require('fs');
 var jade = require('gulp-jade');
+var data = require('gulp-data');
 var jadeInheritance = require('gulp-jade-inheritance');
 var filter = require('gulp-filter');
 
 var jadeSrc = './' + config.src + 'views/';
 
+
 $.gulp.task('views', function() {
     return $.gulp.src(jadeSrc + '**/*.jade')
         .pipe($.plumber({ errorHandler: $.notify.onError('<%= error.message %>') }))
         .pipe(jadeInheritance({ basedir: jadeSrc }))
+        .pipe(data(function() {
+            return {
+                production: config.prod,
+                file: JSON.parse(fs.readFileSync('./source/data.json'))
+            };
+        }))
         .pipe(jade({
             basedir: jadeSrc,
-            pretty: true,
-            locals: { production: config.prod }
+            pretty: true
         }))
         .pipe(filter(function(file) {
             var exclude;
